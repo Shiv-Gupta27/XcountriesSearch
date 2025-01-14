e92f3cffabed9e749faa19224b0edc8bd1e3e4d8
unknown <shivprasadgupta056@gmail.com>
2025-01-15 00:25:50 +0530

    updated all the code with in the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import Searchcountry from './component/Searchcountry';
+
 import './App.css';
+
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
 
+  const [SearchText, SetSearchText] = useState("")
+  const [countryList, setCountryList] = useState([]);
+  const [filterCountry, setFilterCountry] = useState([]);
+
+  useEffect(
+      ()=>{
+          const fetchCard = async ()=>{
+              try{
+                  const response = await axios.get("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries");
+                  setCountryList(response.data)
+                  setFilterCountry(response.data)
+              }catch(e){
+                  console.error(e);
+                  setCountryList([])
+              }
+              
+          }
+
+          fetchCard();
+      },[]
+  );
+
+  useEffect(()=>{
+      if (SearchText){
+          const filterCountry = countryList.filter((ele)=>ele.common.toLowerCase().includes(SearchText.toLowerCase()))
+          setFilterCountry(filterCountry);
+      }else{
+          setFilterCountry(countryList);
+      }
+  },[SearchText]);
+
   return (
     <div className="App">
-      <Searchcountry />
+      <div>
+          <input type="text" className="Search" value={SearchText} onChange={(e)=>{SetSearchText(e.target.value)}} name="searchbox" />
+      </div>
+
+      <div className="containner">
+          {
+              filterCountry?filterCountry.map((ele)=>{
+                  return (
+                    <div className="countryCard">
+                      <div>
+                          <img src={ele.png} className="CardImg" alt={ele.common} />
+                      </div>
+                      <p>{ele.common}</p>
+                    </div>
+                  )
+              }):<></>
+          }
+      </div>
     </div>
   );
 }
--- src/component/Searchcountry.jsx
+++ src/component/Searchcountry.jsx
@@ -1,54 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Countrycard from "./CountryCard/CountryCard";
-import axios from "axios"
-import Styles from "./Searchcountry.module.css";
+
 function Searchcountry(){
 
-    const [SearchText, SetSearchText] = useState("")
-    const [countryList, setCountryList] = useState([]);
-    const [filterCountry, setFilterCountry] = useState([]);
-
-    useEffect(
-        ()=>{
-            const fetchCard = async ()=>{
-                try{
-                    const response = await axios.get("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries");
-                    setCountryList(response.data)
-                    setFilterCountry(response.data)
-                }catch(e){
-                    console.error(e);
-                    setCountryList([])
-                }
-                
-            }
-
-            fetchCard();
-        },[]
-    );
-
-    useEffect(()=>{
-        if (SearchText){
-            const filterCountry = countryList.filter((ele)=>ele.common.toLowerCase().includes(SearchText.toLowerCase()))
-            setFilterCountry(filterCountry);
-        }else{
-            setFilterCountry(countryList);
-        }
-    },[SearchText]);
+   
 
 
     return(
         <div>
-            <div>
-                <input type="text" className={Styles.Search} value={SearchText} onChange={(e)=>{SetSearchText(e.target.value)}} name="searchbox" />
-            </div>
-
-            <div className={Styles.containner}>
-                {
-                    filterCountry?filterCountry.map((ele)=>{
-                        return <Countrycard countryimg = {ele.png} countryname = {ele.common} />       
-                    }):<></>
-                }
-            </div>
+            
 
         </div>
     )
